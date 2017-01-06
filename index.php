36c7171b8cbb515d9ceada7ab352e07985b488c9
ankitaggarwal1990 <aggarwal.ankit007@gmail.com>
2017-01-06 12:50:43 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,19 @@
 <?php
 	echo "hello";
 
+// PHP Data Objects(PDO) Sample Code:
+try {
+    $conn = new PDO("sqlsrv:server = tcp:trident1.database.windows.net,1433; Database = database_azure", "trident", "ankit@123");
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}
+catch (PDOException $e) {
+    print("Error connecting to SQL Server.");
+    die(print_r($e));
+}
+
+// SQL Server Extension Sample Code:
+$connectionInfo = array("UID" => "trident@trident1", "pwd" => "ankit@123", "Database" => "database_azure", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
+$serverName = "tcp:trident1.database.windows.net,1433";
+$conn = sqlsrv_connect($serverName, $connectionInfo);
+
 ?>
