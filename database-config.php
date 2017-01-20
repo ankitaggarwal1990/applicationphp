a6fb23ddb7a3fa15ce29537a84e716d2e1188178
ankitaggarwal1990 <aggarwal.ankit007@gmail.com>
2017-01-20 16:53:25 +0530

    Update database-config.php

--- database-config.php
+++ database-config.php
@@ -17,5 +17,25 @@ else
 {
    echo "Connect";
 }
+
+
+
+// PHP Data Objects(PDO) Sample Code:
+try {
+    $conn = new PDO("sqlsrv:server = tcp:trident1.database.windows.net,1433; Database = database_azure", "trident", "tgbs2016");
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}
+catch (PDOException $e) {
+    print("Error connecting to SQL Server.");
+    die(print_r($e));
+}
+
+// SQL Server Extension Sample Code:
+$connectionInfo = array("UID" => "trident@trident1", "pwd" => "tgbs2016", "Database" => "database_azure", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
+$serverName = "tcp:trident1.database.windows.net,1433";
+$conn = sqlsrv_connect($serverName, $connectionInfo);
+
+
+
    
 ?>
