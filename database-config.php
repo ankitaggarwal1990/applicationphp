540b5d74a20b73eec09efc8075ac0ced92ee3120
ankitaggarwal1990 <aggarwal.ankit007@gmail.com>
2017-01-24 11:32:33 +0530

    Update database-config.php

--- database-config.php
+++ database-config.php
@@ -1,24 +1,5 @@
 <?php
-   // define database related variables
-   $database = 'database';
-   $host = 'localhost';
-   $user = 'root';
-   $pass = '';
-
-echo "sadas";
-   // try to conncet to database
-   $dbh = new PDO("mysql:dbname={$database};host={$host};port={3306}", $user, $pass);
-
-   if(!$dbh){
-
-      echo "unable to connect to database";
-   }
-else
-{
-   echo "Connect";
-}
-
-
+  
 
 // PHP Data Objects(PDO) Sample Code:
 try {
@@ -34,6 +15,15 @@ catch (PDOException $e) {
 $connectionInfo = array("UID" => "trident@trident1", "pwd" => "tgbs2016", "Database" => "database_azure", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
 $serverName = "tcp:trident1.database.windows.net,1433";
 $conn = sqlsrv_connect($serverName, $connectionInfo);
+
+if($conn)
+{
+ echo"1";  
+}
+else{
+   echo "2";
+   
+}
 
 
 
