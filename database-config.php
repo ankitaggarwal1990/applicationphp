eeff8d99b5bbc9a7a552321dba8f89edfb34735b
ankitaggarwal1990 <aggarwal.ankit007@gmail.com>
2017-01-24 15:27:08 +0530

    Update database-config.php

--- database-config.php
+++ database-config.php
@@ -20,10 +20,10 @@ foreach ($_SERVER as $key => $value) {
     $connectstr_dbname = preg_replace("/^.*Database=(.+?);.*$/", "\\1", $value);
     $connectstr_dbusername = preg_replace("/^.*User Id=(.+?);.*$/", "\\1", $value);
     $connectstr_dbpassword = preg_replace("/^.*Password=(.+?)$/", "\\1", $value);
-  echo $connectstr_dbhost "\n";
-  echo $connectstr_dbname "\n";
-  echo $connectstr_dbusername "\n"; 
-  echo $connectstr_dbpassword "\n";
+  echo $connectstr_dbhost ;
+  echo $connectstr_dbname ;
+  echo $connectstr_dbusername ; 
+  echo $connectstr_dbpassword ;
 }
 
 $link = mysqli_connect($connectstr_dbhost, $connectstr_dbusername, $connectstr_dbpassword,$connectstr_dbname);
