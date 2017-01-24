a6c7e22ede217b6590587ac1d7012910c1f24e6c
ankitaggarwal1990 <aggarwal.ankit007@gmail.com>
2017-01-24 12:21:08 +0530

    Update database-config.php

--- database-config.php
+++ database-config.php
@@ -2,31 +2,6 @@
   
 
 // PHP Data Objects(PDO) Sample Code:
-try {
-    $conn = new PDO("sqlsrv:server = tcp:trident1.database.windows.net,1433; Database = database_azure", "trident", "");
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-}
-catch (PDOException $e) {
-    print("Error connecting to SQL Server.");
-    die(print_r($e));
-}
-
-// SQL Server Extension Sample Code:
-$connectionInfo = array("UID" => "trident@trident1", "pwd" => "", "Database" => "database_azure", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
-$serverName = "tcp:trident1.database.windows.net,1433";
-$conn = sqlsrv_connect($serverName, $connectionInfo);
-
-if($conn)
-{
- echo"1";  
-}
-else{
-   echo "2";
-   
-}
-
-
-
    
 ?>
 <?php
@@ -59,5 +34,6 @@ if (!$link) {
 echo "Success: A proper connection to MySQL was made! The my_db database is great." . PHP_EOL;
 echo "Host information: " . mysqli_get_host_info($link) . PHP_EOL;
 
+echo "hghjfhj";
 mysqli_close($link);
 ?>
