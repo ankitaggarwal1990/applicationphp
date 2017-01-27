4187be54959368102860b47468085779ac368fe6
ankitaggarwal1990 <aggarwal.ankit007@gmail.com>
2017-01-27 11:14:57 +0530

    Update nosqldb.php

--- nosqldb.php
+++ nosqldb.php
@@ -1,22 +1,23 @@
 <?php
+echo "Connection to database successfully";
    // connect to mongodb
-   $m = new MongoClient();
-   echo "Connection to database successfully";
+//    $m = new MongoClient();
+//    echo "Connection to database successfully";
 	
-   // select a database
-   $db = $m->mydb;
-   echo "Database mydb selected";
-   $collection = $db->mycol;
-   echo "Collection selected succsessfully";
+//    // select a database
+//    $db = $m->mydb;
+//    echo "Database mydb selected";
+//    $collection = $db->mycol;
+//    echo "Collection selected succsessfully";
 	
-   $document = array( 
-      "title" => "MongoDB", 
-      "description" => "database", 
-      "likes" => 100,
-      "url" => "http://www.tutorialspoint.com/mongodb/",
-      "by", "tutorials point"
-   );
+//    $document = array( 
+//       "title" => "MongoDB", 
+//       "description" => "database", 
+//       "likes" => 100,
+//       "url" => "http://www.tutorialspoint.com/mongodb/",
+//       "by", "tutorials point"
+//    );
 	
-   $collection->insert($document);
-   echo "Document inserted successfully";
+//    $collection->insert($document);
+//    echo "Document inserted successfully";
 ?>
