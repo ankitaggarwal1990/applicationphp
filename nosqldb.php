82a76992ca65d9325b1e7bd609812fcb1c51d11f
ankitaggarwal1990 <aggarwal.ankit007@gmail.com>
2017-01-27 11:24:02 +0530

    Update nosqldb.php

--- nosqldb.php
+++ nosqldb.php
@@ -1,7 +1,7 @@
 <?php
 echo "Connection to database successfully";
    // connect to mongodb
-    $m = new MongoClient();
+    $m = new Mongo();
     echo "Connection to database successfully";
 	
 //    // select a database
