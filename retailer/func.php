a8613ff68a0dafdce3b56a913c3e6e0c0af330cf
ankitaggarwal1990 <aggarwal.ankit007@gmail.com>
2017-02-16 17:59:52 +0530

    Update func.php

--- retailer/func.php
+++ retailer/func.php
@@ -1,7 +1,7 @@
 <?php 
 print_r($_POST);
 
-$con = mysqli_connect('localhost','root','','database');
+$con = mysqli_connect('127.0.0.1:49646','azure','6#vWHD_$','localdb');
 if (!$con) {
     die('Could not connect: ' . mysqli_error($con));
 }
