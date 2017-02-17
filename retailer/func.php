1b4272893795d5762b75ad2b8a60bf7839c1a150
ankitaggarwal1990 <aggarwal.ankit007@gmail.com>
2017-02-17 21:04:46 +0530

    Update func.php

--- retailer/func.php
+++ retailer/func.php
@@ -35,7 +35,7 @@ while($row = mysqli_fetch_array($result)) {
 	if(isset($_POST[$product])){
 		$quantity1 = "quantity".$_POST[$product];
 		echo $quantity = $_POST[$quantity1];
-		$sql = "INSERT INTO `orders`(`order_id`, `product_id`, `quantity`, `rfid_tag_id`, `destination_id`, `transport_id`, `delivery_time`, `product_total_amount`) VALUES (3,3,3,3,3,3,NOW(),3)";
+		$sql = "INSERT INTO `orders`(`order_id`, `product_id`, `quantity`, `rfid_tag_id`, `destination_id`, `transport_id`, `delivery_time`, `product_total_amount`) VALUES ('$orderid','$product','$quantity',0,0,0,NOW(),0)";
 		//$query = "INSERT INTO `orders`(`order_id`, `product_id`, `quantity`, `rfid_tag_id`, `destination_id`, `delivery_time`, `transport_id`, `product_total_amount`) VALUES ('$orderid','$product','$quantity',1,1,1,now(),1)"
 		$result1 = mysqli_query($con,$sql);
 		
