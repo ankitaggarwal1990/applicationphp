cb8c353ed17ed9e89838d22e2afaf3bbdfa81a44
ankitaggarwal1990 <aggarwal.ankit007@gmail.com>
2017-02-17 17:37:03 +0530

    Update func.php

--- retailer/func.php
+++ retailer/func.php
@@ -12,7 +12,19 @@ else{
 
 if(isset($_POST['place_order'])){
 
-	echo "asdfghjkl";
+	
+	$sql="SELECT * FROM products";
+
+$result = mysqli_query($con,$sql);
+
+
+$query2 = "SELECT * FROM `orders` WHERE order_id=(SELECT MAX(order_id) FROM `orders`)";
+$result2 = mysqli_query($con,$query2);
+$orderid =0;
+while($row2 = mysqli_fetch_array($result2)) {
+ echo $orderid = $row2['order_id'];
+}
+	//echo "asdfghjkl";
 }
 
 
