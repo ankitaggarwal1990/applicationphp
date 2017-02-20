3d53bd42309f349add79f7bf179e3af4be133315
ankitaggarwal1990 <aggarwal.ankit007@gmail.com>
2017-02-20 12:14:11 +0530

    Update abc.php

--- abc.php
+++ abc.php
@@ -1,3 +1,5 @@
+
+<?php
 $con = mysqli_connect('trident1.database.windows.net','trident','password@123','database_azure');
 if (!$con) {
     die('Could not connect: ' . mysqli_error($con));
@@ -5,3 +7,5 @@ if (!$con) {
 else{
 	echo "connect";
 }
+
+?>
