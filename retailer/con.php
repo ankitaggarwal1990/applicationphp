e98ba0c11ac0fc0617edc2f09b98878781a7107d
ankitaggarwal1990 <aggarwal.ankit007@gmail.com>
2017-02-20 11:27:06 +0530

    Create con.php

---
+++ retailer/con.php
@@ -0,0 +1,7 @@
+$con = mysqli_connect('trident1.database.windows.net','trident','password@123','database_azure');
+if (!$con) {
+    die('Could not connect: ' . mysqli_error($con));
+}
+else{
+	echo "connect";
+}
